28a56861f9dde3a9392ed806bddf515f2afe7e76
marcin walendzik <marcin.walendzik@gmail.com>
2015-04-25 19:24:22 +0100

    adding routers

--- proto1/server.js
+++ proto1/server.js
@@ -1,16 +1,43 @@
 var express = require('express');
 var SFsimulator = require('./helpers/SFsimulator');
+var path = require('path');
+var http = require('http');
 var app = express();
-var server = require('http').createServer(app);
+var server = http.createServer(app);
+
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var expressLayouts = require('express-ejs-layouts');
+
+//router
+var index = require('./routes/index');
+var users = require('./routes/users');
+var admin = require('./routes/admin');
 
 
-app.get('/',function(request,response){
-  response.send("hello");
-});
+//server set-up
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+app.use(expressLayouts);
 
-app.get('/:user',function(request,response){
-  response.send(user);
-});
+
+
+
+
+app.set('views',path.join(__dirname,'app_server','views'));
+app.set('port',(process.env.PORT || 3000));
+
+//sends to router index
+app.use('/',index);
+
+// app.get('/',function(request,response){
+//   response.send("hello");
+// });
+
+// app.get('/:user',function(request,response){
+//   response.send(user);
+// });
 
 server.listen(3000,function(){
   console.log('server is running on port 3000');
