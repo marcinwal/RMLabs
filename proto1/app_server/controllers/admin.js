65a8d5f76a0bff5b53a02e3763281dc33938bec1
marcin walendzik <marcin.walendzik@gmail.com>
2015-04-27 15:41:50 +0100

    adding mixim funxtion for outputCPD

--- proto1/app_server/controllers/admin.js
+++ proto1/app_server/controllers/admin.js
@@ -3,9 +3,10 @@ var SFsimulator = require('../../helpers/SFsimulator');
 module.exports.admin_id = function(request,response){
   var id = request.params.id;
   console.log(id);
-  response.render('admin',{user: SFsimulator(id),title: 'admin page'});
+  response.render('admin',{users: SFsimulator(id),title: 'admin page'});
 };
 
 module.exports.admin = function(request,response){
- response.render('admin',{user: SFsimulator(),title: 'admin page'});  
+ response.render('admin',{users: SFsimulator(),title: 'admin page',                         
+ });  
 }
--- proto1/app_server/controllers/main.js
+++ proto1/app_server/controllers/main.js
@@ -1,3 +1,9 @@
 module.exports.index = function(request,response){
-  response.render('index',{title: 'hello home page'});
+  response.render('index',{
+    title: 'Home page',
+    pageHeader: {
+      title: 'CPD software',
+      strapline: 'Control CPD points of your team'
+    }
+  });
 };
