ada31fd88578df75b97954e5431619e8d903c7da
marcin walendzik <marcin.walendzik@gmail.com>
2015-04-27 16:20:51 +0100

    course updated with linked data

--- proto1/app_server/controllers/admin.js
+++ proto1/app_server/controllers/admin.js
@@ -2,11 +2,9 @@ var SFsimulator = require('../../helpers/SFsimulator');
 
 module.exports.admin_id = function(request,response){
   var id = request.params.id;
-  console.log(id);
   response.render('admin',{users: SFsimulator(id),title: 'admin page'});
 };
 
 module.exports.admin = function(request,response){
- response.render('admin',{users: SFsimulator(),title: 'admin page',                         
- });  
+ response.render('admin',{users: SFsimulator(),title: 'admin page'});  
 }
--- proto1/app_server/controllers/courseInfo.js
+++ proto1/app_server/controllers/courseInfo.js
@@ -1,7 +1,15 @@
 module.exports.courseInfo = function(request,response){
-  response.render('course-info',{title: 'Course info'});
+  response.render('course-info',{title: 'Course info',
+    courseDetails:{
+      rating: 3,
+      address: '120 River road',
+      levels: [2,5,7],
+      dates: ['april','september','december'],
+      location: [51.444034,-0.2913375,13]
+    }});
 };
 
 module.exports.courseNew = function(request,response){
-  response.render('course-new',{title: 'Adding new course'});
+  response.render('course-new',{title: 'Adding new course'
+  });
 };
