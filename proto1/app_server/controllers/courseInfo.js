7b9f15d612d2b07d511966a62a3e041b9de51e16
marcin walendzik <marcin.walendzik@gmail.com>
2015-04-27 12:45:20 +0100

    adding new course layout

--- proto1/app_server/controllers/courseInfo.js
+++ proto1/app_server/controllers/courseInfo.js
@@ -1,3 +1,7 @@
 module.exports.courseInfo = function(request,response){
   response.render('course-info',{title: 'Course info'});
 };
+
+module.exports.courseNew = function(request,response){
+  response.render('course-new',{title: 'Adding new course'});
+};
--- proto1/routes/course.js
+++ proto1/routes/course.js
@@ -3,5 +3,6 @@ var router = express.Router();
 var ctrlCourse = require('../app_server/controllers/courseInfo');
 
 router.get('/',ctrlCourse.courseInfo);
+router.get('/new',ctrlCourse.courseNew);
 
 module.exports = router;
