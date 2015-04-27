5a612ba11bd6f47000b8c15669af3bcab93cd9b9
marcin walendzik <marcin.walendzik@gmail.com>
2015-04-27 19:10:05 +0100

    moongose connection working

--- proto1/app_server/models/db.js
+++ proto1/app_server/models/db.js
@@ -4,3 +4,15 @@ mongoose.connect(dbURI);
 // if(process.env.NODE_ENV === 'production'){
 //   dbURL = process.env.MONGOLAB_URI_CPD;
 // }
+
+mongoose.connection.on('connected',function(){
+  console.log('Moongose connected to'+dbURI);
+});
+
+mongoose.connection.on('error',function(err){
+  console.log('Moongose connection error'+err);
+});
+
+mongoose.connection.on('disconnected',function(){
+  console.log('Moongose disconnected');
+});
